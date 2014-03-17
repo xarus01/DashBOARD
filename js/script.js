f6d03bad2f76c3622e540a208c2ad43ec85445fb
xarus01 <xarus01@gmail.com>
2014-03-18 02:15:31 +0900

    1.0

--- js/script.js
+++ js/script.js
@@ -5,20 +5,22 @@ $(function(){ //DOM Ready
     widget_base_dimensions: [60, 30]
   }).data('gridster');
   var widgets = [
-    ['<li><a href="http://facebook.com/xarus01"><i class="fa fa-facebook-square" style="font-size: 54px;"></i></a></li>', 2, 3],
-    ['<li><a href="http://twitter.com/xarus01"><i class="fa fa-twitter-square" style="font-size: 54px;"></i></a></li>', 2, 3],
-    ['<li><a href="http://k001kat.tumblr.com/"><i class="fa fa-tumblr-square" style="font-size: 54px;"></i></a></li>', 2, 3],
-    ['<li><a href="http://instagram.com/xarus01"><i class="fa fa-instagram" style="font-size: 54px;"></i></a></li>', 2, 3],
-    ['<li><a href="http://koolkat.wo.gl/blog"><i class="fa fa-pencil-square" style="font-size: 54px;"></i></a></li>', 2, 3],
-    ['<li><a href="http://github.com/xarus01"><i class="fa fa-github-square" style="font-size: 54px;"></i></a></li>', 2, 3],
-    ['<li><a href="http://koolkat.wo.gl/map"><i class="fa fa-check-square" style="font-size: 54px;"></i></a></li>', 2, 3]
+    ['<li><a href="http://github.com/xarus01"><i class="fa fa-github-square" style="font-size: 54px;"></i><p>Github</p></a></li>', 1, 3],
+    ['<li><a href="http://facebook.com/xarus01"><i class="fa fa-facebook-square" style="font-size: 54px;"></i><p>Facebook</p></a></li>', 2, 3],
+    ['<li><a href="http://twitter.com/xarus01"><i class="fa fa-twitter-square" style="font-size: 54px;"></i><p>Twitter</p></a></li>', 2, 3],
+    ['<li><a href="http://k001kat.tumblr.com/"><i class="fa fa-tumblr-square" style="font-size: 54px;"></i><p>Tumblr</p></a></li>', 2, 3],
+    ['<li><a href="http://instagram.com/xarus01"><i class="fa fa-instagram" style="font-size: 54px;"></i><p>Instagram</p></a></li>', 3, 5],
+    ['<li><a href="http://koolkat.wo.gl/blog"><i class="fa fa-pencil-square" style="font-size: 54px;"></i><p>WordPress</p></a></li>', 2, 3]
   ];
 
   $.each(widgets, function(i, widget){
     gridster.add_widget.apply(gridster, widget)
   });
 
+//  for feeds
   renderInstagram();
+  renderTumblr();
+  renderWPFeed();
 });
 
 $(window).scroll(function (event) {
@@ -53,37 +55,33 @@ function renderInstagram() {
       var mediaJson = res.data;
       $(mediaJson).each(function(){
         if($(this)[0].type == "image") {
-          console.log($(this)[0].images.thumbnail.url);
-//          post.images.thumbnail //url, width, height, maybe use low_resolution?
-          //Do
+          $("#insta").append('<div class="col-xs-6 col-md-3"><a href="'+$(this)[0].link+'" class="thumbnail"><img src="'+$(this)[0].images.thumbnail.url+'"></a></div>');
         }
       });
     });
   });
 }
-
-/*
-//instagram photos
-$.get("page", function(res) {
-    $.get("page_with_data", function(res) {
-        var mediaJson = $.parseJSON(res).data;
-        $(mediaJson).each(function(post){
-            if(post.type == "image") {
-                post.images.thumbnail //url, width, height, maybe use low_resolution?
-                //Do
-            }
-        });
+  
+function renderTumblr() {
+  var apiKey = "HCoCFhyXyzTRYdDoVGy0bcbv3oqk3cvKRUNP5yYD4QuyuzkgP9"
+  $.getJSON("http://api.tumblr.com/v2/blog/k001kat.tumblr.com/posts/text?api_key="+apiKey+"&callback=?", function(res) {
+    var posts = res.response.posts;
+    $(posts).each(function(i) {
+      var p = $.parseHTML($(this)[0].body);
+//      show <p> only
+      $("#tumbl").append('<blockquote class="blockquote-reverse" id="li_'+i+'"></blockquote>');
+      $("#tumbl").find("#li_"+i).append($(p)[0]);
+      $("#tumbl").find("#li_"+i).append('<footer><a href="'+$(this)[0].post_url+'">'+$(this)[0].date+'</a></footer>');
     });
-});
+  });
+}
 
 //WP feed
-$.get("http://koolkat.wo.gl/blog/feed/", function(res) {
-    var wpFeed = $.parseXML(res);
-    $(wpFeed).find("item").each(function(i) {
-        i.title
-        i.link
-        i.description //<![CDATA ?
-        //DO
+function renderWPFeed() {
+  $.getJSON("http://koolkat.wo.gl/blog/?feed=json&callback=?", function(res) {
+    $(res).each(function() {
+      $("#wp").append('<li><h3><a href="'+$(this)[0].permalink+'">'+$(this)[0].title+'</a></h3><p>'+$(this)[0].content+'</p></li>');
+//      console.log($(this)[0].title);
     });
-});
-*/
+  });
+}
